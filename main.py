a0d199ecfa1e537289fc9b8bc3db6c36ac9e16ee
sibaprasad <sibap865@gmail.com>
2023-08-02 11:21:30 +0530

    data vaildation added

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from mlflowproject import logger
-from mlflowproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlflowproject import logger
+from src.mlflowproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlflowproject.pipeline.stage_02_datavalidation  import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Data Validation stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
