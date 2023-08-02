40014a9820af299a74573f1db4f009daa9090707
sibaprasad <sibap865@gmail.com>
2023-08-02 12:35:54 +0530

    data transformation and model trainer added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from src.mlflowproject import logger
-from src.mlflowproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from src.mlflowproject.pipeline.stage_02_datavalidation  import DataValidationTrainingPipeline
+from mlflowproject import logger
+from mlflowproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlflowproject.pipeline.stage_02_datavalidation  import DataValidationTrainingPipeline
+from mlflowproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlflowproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 STAGE_NAME = "Data Validation stage"
+STAGE_NAME = "Data Transformation stage"
+
+
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -22,3 +27,24 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
